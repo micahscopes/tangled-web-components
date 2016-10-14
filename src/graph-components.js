83226ac10fb000779d68ad80f4fc638e29893f62
Micah <fitchmicah@gmail.com>
2016-10-13 20:53:15 -0500

    it's workin

--- src/graph-components.js
+++ src/graph-components.js
@@ -1,13 +1,7 @@
-// import {math} from 'mathjs'
-import {fromEvent} from 'rxjs/observable/fromEvent';
-import 'rxjs/add/operator/debounce';
-
 import {
-  event,
   select,
   selectAll
 } from "d3-selection";
-import { zip } from "d3-array"
 import { line } from "d3-shape"
 import cmb from "js-combinatorics"
 import { define, h, prop } from 'skatejs';
@@ -15,11 +9,12 @@ import * as skate from 'skatejs';
 const sym = Symbol();
 
 var css=`
-path {stroke: #0f0; stroke-width:2px;fill:none}
+path {stroke: #0f0; stroke-width:4px; fill:none}
 svg {
   width: 97%;
   height: 97%;
   position: absolute;
+  z-index: -1;
 }
 `
 
@@ -33,60 +28,38 @@ var shadowSVGSelector = (elem) => { var sdw = elem.shadowRoot;
                                        else { return select() }
 };
 
-var animate = (elem) => {function(elem){
-  shadowSVGSelector(elem).selectAll("path")
-      .attr("d",drawEdge)
-  window.requestAnimationFrame( animate(elem) );
-}}
+var animate = (elem) => function(){
+  // a hack, i don't know how to listen for shadowDOM to be ready the first time...
+  var edgeLines = shadowSVGSelector(elem).selectAll("path")
+      .data(elem.pairs)
+  edgeLines.exit().remove();
+  edgeLines.enter().append("path");
+  edgeLines.attr("d",drawEdge);
+  setTimeout( () => { window.requestAnimationFrame( animate(elem) ) }, 1000/elem.fps );
+}
 
 var updateNodes = function(elem){
   var nodes = select(elem).selectAll("*").nodes()
   console.log("updateNodes",nodes)
-  nodes.forEach(function(node){
-    var positionChanges = fromEvent(node,"moved");
-    elem.positionObserver.observe(node, {
-        attributes: true,
-        attributeFilter: ['offsetLeft','offsetTop'],
-    });
-    positionChanges.debounce(500).subscribe(() => {updateEdgeSegments(node)});
-    positionChanges.subscribe(() => {console.log(node,"moved")});
-  });
   if (nodes.length < 2) { return; }
-  var pairs = cmb.combination(nodes,2).toArray()
-  var edgeLines = shadowSVGSelector(elem).selectAll("path")
-      .data(pairs)
-  // console.log(edgeLines)
-  // console.log(pairs)
-  edgeLines.exit().remove();
-  edgeLines.enter().append("path");
-  updateEdgeSegments(elem);
+  elem.pairs = cmb.combination(nodes,2).toArray()
 }
 
 const mainSlot = Symbol();
 define('graph-viewer',
  {
-  props: {
-    count: prop.number({ attribute: true })
-  },
-  attached(elem) {
-    elem.updateNodes = updateNodes;
-    elem.updateEdgeSegments = updateEdgeSegments;
+   props: {
+     fps: { attribute: true, default: 60 }
+   },
+   attached(elem) {
     console.log("graph-viewer attached")
-    elem.positionObserver = new MutationObserver(function(mutation){ console.log(mutation) });
-    // elem[sym] = setInterval(() => ++elem.count, 1000);
-    // elem.graphNodes = () => {
-    //   console.log(elem[mainSlot]);
-    //   elem[mainSlot].assignedNodes()}
+    updateNodes(elem); // doesn't seem to be working
+    animate(elem)();
   },
-  detached(elem) {
-    clearInterval(elem[sym]);
+  attributeChanged(elem) {
+    updateNodes(elem); // doesn't seem to be working
   },
   render(elem) {
-    // console.log("ready")
-    // console.log(elem[skate.symbols.shadowRoot]);
-    // console.log(select(elem[skate.symbols.shadowRoot]))
-    // console.log(select(elem[skate.symbols.shadowRoot]).select("slot"))
-    updateEdgeSegments(elem);
     return [
       h('svg',{id:"abc"}),
       h('slot', {
